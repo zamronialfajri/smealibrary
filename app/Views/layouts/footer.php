4bb2a6f2f57dd8905d9d2826909cf398877bce25
zamronialfajri <zamronialfajri0@gmail.com>
2025-03-07 10:08:10 +0700

    first commit smealibrary

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,3 @@
+<div class="py-6 px-6 text-center">
+  <p class="mb-0 fs-4">&copy; 2025, made with by <a href="https://www.instagram.com/heyrii__/" target="_blank" class="pe-1 text-primary text-decoration-underline">Zamroni Alfajri</a></p>
+</div>
